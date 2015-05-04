1e1b05b0866e8c81daf126869a592297a84d208e
Amandla Beautin <amandlabeautin@gmail.com>
2015-05-04 15:56:38 +0200

    Test complet de la classe ArrayStock

--- MOE(Puissance 4)/Puissance4/Power4Test/ArrayStockTest.cs
+++ MOE(Puissance 4)/Puissance4/Power4Test/ArrayStockTest.cs
@@ -13,15 +13,18 @@ namespace Power4Test
         [TestMethod]
         public void addToken()
         {
+
             IToken empty = new Token();
             IToken plein = new Token('+', 'O', ConsoleColor.Yellow);
             
             ArrayStock tableau = new ArrayStock(10, 10, empty);
             IPlayer player = new Player(plein, 10);
-
+            
+            /* Si le jeton est en dehors du tableau */
             int solution = tableau.addToken(player, 15);
             Assert.AreEqual(-1, solution);
-
+            
+            /* Si Le tableau est rempli */
             ArrayStock tableauPlein = new ArrayStock(10, 10, plein);
             int TokenPlein = tableauPlein.addToken(player, 9);
             Assert.AreNotEqual(-1, TokenPlein);
