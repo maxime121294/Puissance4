c2f58346046371a94f95fed4b22af67863351645
Amandla Beautin <amandlabeautin@gmail.com>
2015-05-11 07:06:02 +0200

    Ajout du container

---
+++ MOE(Puissance 4)/Puissance4/Power4Test/ContainerTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Power4;
+using Autofac;
+
+namespace Power4Test
+{
+    [TestClass]
+    public class ContainerTest
+    {
+        [TestMethod]
+        public Autofac.IContainer CreateContainer(Action<ContainerBuilder> configure = null)
+        {
+            
+            ContainerBuilder builder = new ContainerBuilder();
+            
+            builder.RegisterType<ArrayFormat>().As<IArrayFormatter>().SingleInstance();
+            builder.RegisterType<ArrayStock>().As<IArrayStock>().SingleInstance();
+            builder.RegisterType<KeyboardInput>().As<IInput>().SingleInstance();
+            builder.RegisterType<Iterator>().As<IIterator>().SingleInstance();
+            builder.RegisterType<ColorOutput>().As<IOutput>().SingleInstance();
+            builder.RegisterType<Player>().As<IPlayer>().SingleInstance();
+            builder.RegisterType<Token>().As<IToken>().SingleInstance();
+
+
+            if (configure != null)
+                configure(builder);
+
+            var container = builder.Build();
+            return container;
+        }
+    }
+}
